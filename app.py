ae619079b733c1635b3c8d2af68102af80b30f9a
nchitere <97787733+nchitere@users.noreply.github.com>
2023-09-27 18:36:33 +0000

    Changed name from Jovian to Data Dabbler

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-jobs =[
-  {'id' : 1,
-  'title':'Data analyst',
-  'location':'Nairobi,Kenya',
-  'salary':'Ksh 350,000'},
-    {'id' : 2,
-  'title':'Data scientist',
-  'location':'Nairobi,Kenya',
-  'salary':'Ksh 500,000'},
-    {'id' : 3,
-  'title':'Front end Engineer',
-  'location':'Remote',
-  'salary':'Ksh 1050,000'},
-      {'id' : 4,
-  'title':'Back end Engineer',
-  'location':'Montana US',
-  'salary':'$ 300,000'}
-]
+jobs = [{
+    'id': 1,
+    'title': 'Data analyst',
+    'location': 'Nairobi,Kenya',
+    'salary': 'Ksh 350,000'
+}, {
+    'id': 2,
+    'title': 'Data scientist',
+    'location': 'Nairobi,Kenya',
+    'salary': 'Ksh 500,000'
+}, {
+    'id': 3,
+    'title': 'Front end Engineer',
+    'location': 'Remote',
+    'salary': 'Ksh 1050,000'
+}, {
+    'id': 4,
+    'title': 'Back end Engineer',
+    'location': 'Montana US',
+    'salary': '$ 300,000'
+}]
+
 
 @app.route("/")
 def Data_Engineer():
-  return render_template('home.html',jobs=jobs, company_name ="Jovian")
+  return render_template('home.html', jobs=jobs, company_name="Data Dabbler")
 
 
 @app.route("/jobs")
